e064bfcf46b433501e2b14fb14a2b6347703cbaa
Manuel Díez Frutos <manutenfruits@gmail.com>
2015-08-21 02:32:23 +0200

    Fixed identation

--- arduino/Garage/Garage.ino
+++ arduino/Garage/Garage.ino
@@ -1,15 +1,15 @@
- /*
+/*
 
-    An Ethernet web server to allow to open multiple doors using a password-base
-    authentication that relies on HMAC-SHA 256.
+  An Ethernet web server to allow to open multiple doors using a password-base
+  authentication that relies on HMAC-SHA 256.
 
-    Uses a trimmed down fork of Cathedrow's Cryptosuite library (https://github.com/manutenfruits/Cryptosuite)
-    Uses jcw's Ethercard library (https://github.com/jcw/ethercard)
+  Uses a trimmed down fork of Cathedrow's Cryptosuite library (https://github.com/manutenfruits/Cryptosuite)
+  Uses jcw's Ethercard library (https://github.com/jcw/ethercard)
 
-    Forked from jdotjdot's DoorDuino, adapted for the ENC28J60 chip, with enhanced
-    security and more standard HTTP communication
+  Forked from jdotjdot's DoorDuino, adapted for the ENC28J60 chip, with enhanced
+  security and more standard HTTP communication
 
- */
+*/
 
 
 #include <EtherCard.h>
@@ -54,12 +54,12 @@ unsigned long currentNonce = 0;
 unsigned long timerOpenDoor = 0;
 
 struct clientInput {
-    char * time_s;
-    unsigned long time;
-    char * hash_s;
-    char hash[65];
-    char * door_s;
-    int door;
+  char * time_s;
+  unsigned long time;
+  char * hash_s;
+  char hash[65];
+  char * door_s;
+  int door;
 };
 
 //Buffer for responses
@@ -71,198 +71,198 @@ byte Ethernet::buffer[600];
 
 void setup() {
 
-    Serial.begin(57600);
+  Serial.begin(57600);
 
-     while (!Serial) {
-        ; // wait for serial port to connect. Needed for Leonardo only
-    }
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 
-    // Configure door pins as outputs
-    int i;
-    for (i = 0; i < NR_OF_DOORS; i++) {
-        pinMode(doorPins[i], OUTPUT);
-        digitalWrite(doorPins[i], HIGH);
-    }
+  // Configure door pins as outputs
+  int i;
+  for (i = 0; i < NR_OF_DOORS; i++) {
+    pinMode(doorPins[i], OUTPUT);
+    digitalWrite(doorPins[i], HIGH);
+  }
 
-    Serial.println(F("\n[Starting...]"));
+  Serial.println(F("\n[Starting...]"));
 
-    //Initialize Ethernet module
-    if (ether.begin(sizeof Ethernet::buffer, mac) == 0)
-        Serial.println(F("Failed to access Ethernet controller"));
+  //Initialize Ethernet module
+  if (ether.begin(sizeof Ethernet::buffer, mac) == 0)
+  Serial.println(F("Failed to access Ethernet controller"));
 
-    //Try DHCP request
-    if (!ether.dhcpSetup())
-        Serial.println(F("DHCP failed"));
+  //Try DHCP request
+  if (!ether.dhcpSetup())
+  Serial.println(F("DHCP failed"));
 
-    //Print IP information
-    ether.printIp(F("IP address: "), ether.myip);
-    ether.printIp(F("Netmask: "), ether.netmask);
+  //Print IP information
+  ether.printIp(F("IP address: "), ether.myip);
+  ether.printIp(F("Netmask: "), ether.netmask);
 }
 
 //Parse input from client's URL
 struct clientInput parseClientInput(char * url) {
 
-    struct clientInput holder;
+  struct clientInput holder;
 
-    holder.time_s = strtok(url, "/ \n");
-    holder.door_s = strtok(NULL, "/ \n");
-    holder.hash_s = strtok(NULL, "/ \n");
+  holder.time_s = strtok(url, "/ \n");
+  holder.door_s = strtok(NULL, "/ \n");
+  holder.hash_s = strtok(NULL, "/ \n");
 
-    holder.time = strtoul(holder.time_s, NULL, 10);
-    holder.door = strtol(holder.door_s, NULL, 10);
-    strncpy(holder.hash, holder.hash_s, 65);
+  holder.time = strtoul(holder.time_s, NULL, 10);
+  holder.door = strtol(holder.door_s, NULL, 10);
+  strncpy(holder.hash, holder.hash_s, 65);
 
-    return holder;
+  return holder;
 }
 
 boolean toggleDoor(int pin, boolean open) {
 
-    if (open) {
-        if (pin >= 0 && pin < NR_OF_DOORS) {
-            Serial.print(F("Opening door "));
-            Serial.print(pin);
-            Serial.print(F(" on pin "));
-            Serial.println(doorPins[pin]);
+  if (open) {
+    if (pin >= 0 && pin < NR_OF_DOORS) {
+      Serial.print(F("Opening door "));
+      Serial.print(pin);
+      Serial.print(F(" on pin "));
+      Serial.println(doorPins[pin]);
 
-            digitalWrite(doorPins[pin], LOW);
-            return true;
-        } else {
-            Serial.print(F("No door"));
-            Serial.println(pin);
-            return false;
-        }
+      digitalWrite(doorPins[pin], LOW);
+      return true;
     } else {
-        digitalWrite(doorPins[pin], HIGH);
-        return true;
+      Serial.print(F("No door"));
+      Serial.println(pin);
+      return false;
     }
+  } else {
+    digitalWrite(doorPins[pin], HIGH);
+    return true;
+  }
 }
 
 void loop() {
 
-    if (asyncOpenDoor >= 0) {
-        //If door should be opened
-        if (timerOpenDoor == 0) {
-            //If it has not been opened yet
-            if (toggleDoor(asyncOpenDoor, true)) {
-                //If door exists
-                timerOpenDoor = millis();
-            } else {
-                //If it doesn't exist
-                asyncOpenDoor = -1;
-            }
-        } else if (millis() - timerOpenDoor >= OPENING_DELAY){
-            //If it has been opened and the time has passed
-            toggleDoor(asyncOpenDoor, false);
-            asyncOpenDoor = -1;
-            timerOpenDoor = 0;
-        }
+  if (asyncOpenDoor >= 0) {
+    //If door should be opened
+    if (timerOpenDoor == 0) {
+      //If it has not been opened yet
+      if (toggleDoor(asyncOpenDoor, true)) {
+        //If door exists
+        timerOpenDoor = millis();
+      } else {
+        //If it doesn't exist
+        asyncOpenDoor = -1;
+      }
+    } else if (millis() - timerOpenDoor >= OPENING_DELAY){
+      //If it has been opened and the time has passed
+      toggleDoor(asyncOpenDoor, false);
+      asyncOpenDoor = -1;
+      timerOpenDoor = 0;
     }
+  }
 
-    // listen for incoming clients
-    word pos = ether.packetLoop(ether.packetReceive());
-    // check if valid tcp data is received
-    if (pos) {
+  // listen for incoming clients
+  word pos = ether.packetLoop(ether.packetReceive());
+  // check if valid tcp data is received
+  if (pos) {
 
-        Serial.println("new client");
+    Serial.println("new client");
+
+    //Serial.print(F("Available memory: "));
+    //Serial.println(availableMemory());
+
+    // an http request ends with a blank line
+    char* packetData = (char *) Ethernet::buffer + pos;
+    unsigned int packetLength = strlen(packetData);
+    char * method = strtok(packetData, " ");
+    char * url = strtok(NULL, " ");
+
+    ether.httpServerReplyAck(); // send ack to the request
+
+    boolean isPost = strcmp(method, "POST") == 0,
+    isOptions = strcmp(method, "OPTIONS") == 0;
+
+    // Ignore queries while opening
+    if (asyncOpenDoor >= 0) {
+      sendCode(HTTP_420, sizeof HTTP_420, true);
+    } else if (packetLength >= 600) {
+      // flush buffer if goes above 600
+      //I'm a teapot
+      sendCode(HTTP_418, sizeof HTTP_418, true);
+
+    } else if (isOptions) {
+      //Inform about CORS
+      sendCode(HTTP_200, sizeof HTTP_200, true);
+
+    } else if (isPost) {
+
+      //Grab request data
+      struct clientInput parsedInput = parseClientInput(url);
+
+      if (parsedInput.time < currentNonce) {
+
+        const int n = snprintf(NULL, 0, "%lu", currentNonce);
+        int timeLength = snprintf(response, n+1, "%lu", currentNonce);
+
+        sendCode(HTTP_400, sizeof HTTP_400, false);
+        memcpy(ether.tcpOffset(), response, timeLength);
+        ether.httpServerReply_with_flags(timeLength, TCP_FLAGS_ACK_V|TCP_FLAGS_FIN_V);
 
         //Serial.print(F("Available memory: "));
         //Serial.println(availableMemory());
 
-        // an http request ends with a blank line
-        char* packetData = (char *) Ethernet::buffer + pos;
-        unsigned int packetLength = strlen(packetData);
-        char * method = strtok(packetData, " ");
-        char * url = strtok(NULL, " ");
+      } else {
 
-        ether.httpServerReplyAck(); // send ack to the request
+        //update time counter
+        currentNonce = parsedInput.time + 1;
 
-        boolean isPost = strcmp(method, "POST") == 0,
-                isOptions = strcmp(method, "OPTIONS") == 0;
+        // check hash
+        uint8_t *hash;
+        Sha256.initHmac((uint8_t *) password, passwordLen);
+        Sha256.print(parsedInput.time_s);
+        hash = Sha256.resultHmac();
 
-        // Ignore queries while opening
-        if (asyncOpenDoor >= 0) {
-            sendCode(HTTP_420, sizeof HTTP_420, true);
-        } else if (packetLength >= 600) {
-            // flush buffer if goes above 600
-            //I'm a teapot
-            sendCode(HTTP_418, sizeof HTTP_418, true);
+        char hash_c[65];
 
-        } else if (isOptions) {
-            //Inform about CORS
-            sendCode(HTTP_200, sizeof HTTP_200, true);
+        // convert from uint8_t to HEX str
+        String tmpHash;
+        String hash_s = "";
 
-        } else if (isPost) {
-
-            //Grab request data
-            struct clientInput parsedInput = parseClientInput(url);
-
-            if (parsedInput.time < currentNonce) {
-
-                const int n = snprintf(NULL, 0, "%lu", currentNonce);
-                int timeLength = snprintf(response, n+1, "%lu", currentNonce);
-
-                sendCode(HTTP_400, sizeof HTTP_400, false);
-                memcpy(ether.tcpOffset(), response, timeLength);
-                ether.httpServerReply_with_flags(timeLength, TCP_FLAGS_ACK_V|TCP_FLAGS_FIN_V);
-
-                //Serial.print(F("Available memory: "));
-                //Serial.println(availableMemory());
-
-            } else {
-
-                //update time counter
-                currentNonce = parsedInput.time + 1;
-
-                // check hash
-                uint8_t *hash;
-                Sha256.initHmac((uint8_t *) password, passwordLen);
-                Sha256.print(parsedInput.time_s);
-                hash = Sha256.resultHmac();
-
-                char hash_c[65];
-
-                // convert from uint8_t to HEX str
-                String tmpHash;
-                String hash_s = "";
-
-                for (int i=0; i<32; i++) {
-                    tmpHash = String(hash[i], HEX);
-                    while (tmpHash.length() < 2) {
-                        tmpHash = "0" + tmpHash;
-                    }
-                    hash_s += tmpHash;
-                }
-                hash_s.toCharArray(hash_c, 65);
-
-                if (strcmp(parsedInput.hash, hash_c) == 0) {
-
-                    if (parsedInput.door < 0 || parsedInput.door > NR_OF_DOORS) {
-                        sendCode(HTTP_404, sizeof HTTP_404, true);
-                    } else {
-                        asyncOpenDoor = parsedInput.door;
-                        sendCode(HTTP_200, sizeof HTTP_200, true);
-                    }
-                } else {
-                    // don't open door
-                    sendCode(HTTP_401, sizeof HTTP_401, true);
-                }
-            }
-        } else {
-            //Method not implemented
-            sendCode(HTTP_405, sizeof HTTP_405, true);
+        for (int i=0; i<32; i++) {
+          tmpHash = String(hash[i], HEX);
+          while (tmpHash.length() < 2) {
+            tmpHash = "0" + tmpHash;
+          }
+          hash_s += tmpHash;
         }
+        hash_s.toCharArray(hash_c, 65);
+
+        if (strcmp(parsedInput.hash, hash_c) == 0) {
+
+          if (parsedInput.door < 0 || parsedInput.door > NR_OF_DOORS) {
+            sendCode(HTTP_404, sizeof HTTP_404, true);
+          } else {
+            asyncOpenDoor = parsedInput.door;
+            sendCode(HTTP_200, sizeof HTTP_200, true);
+          }
+        } else {
+          // don't open door
+          sendCode(HTTP_401, sizeof HTTP_401, true);
+        }
+      }
+    } else {
+      //Method not implemented
+      sendCode(HTTP_405, sizeof HTTP_405, true);
     }
+  }
 }
 
 void sendCode (const char* code, long length, boolean finish) {
-    memcpy_P(ether.tcpOffset(), code, length);
-    memcpy_P(ether.tcpOffset() + length, CORS_HEADERS, sizeof CORS_HEADERS);
+  memcpy_P(ether.tcpOffset(), code, length);
+  memcpy_P(ether.tcpOffset() + length, CORS_HEADERS, sizeof CORS_HEADERS);
 
-    uint8_t flags = TCP_FLAGS_ACK_V;
-    if (finish) {
-        flags |= TCP_FLAGS_FIN_V;
-    }
-    ether.httpServerReply_with_flags(length + sizeof CORS_HEADERS - 1, flags);
+  uint8_t flags = TCP_FLAGS_ACK_V;
+  if (finish) {
+    flags |= TCP_FLAGS_FIN_V;
+  }
+  ether.httpServerReply_with_flags(length + sizeof CORS_HEADERS - 1, flags);
 }
 
 int availableMemory () {
